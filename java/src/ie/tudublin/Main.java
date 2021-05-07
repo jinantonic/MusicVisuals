db0bd54d8f5c35c580da1af22bc44388361045a4
Jina Park <jinapark119@gmail.com>
2021-05-07 17:51:27 +0100

    bla

--- java/src/ie/tudublin/Main.java
+++ java/src/ie/tudublin/Main.java
@@ -1,6 +1,5 @@
 package ie.tudublin;
 
-import D19123917.Elmo;
 import D19123917.JinasVisual;
 
 
@@ -13,18 +12,9 @@ public class Main
         processing.core.PApplet.runSketch(a, new JinasVisual());		
 	}
 
-	public void elmo()
-	{
-		String[] a = {"MAIN"};
-        processing.core.PApplet.runSketch(a, new Elmo());		
-	}
-
-
-
 	public static void main(String[] args)
 	{
 		Main main = new Main();   
 		main.startUI();	
-		//main.elmo();		
 	}
 }
